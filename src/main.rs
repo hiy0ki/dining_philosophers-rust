bf59157bf5e231cc8521c6a6ca5326becf7bf1ca
hiyoki <maha.tree.21@gmail.com>
2018-01-04 21:23:57 +0900

    add Philosopher struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+struct Philosopher {
+    name: String,
+}
+
+impl Philosopher {
+    // newはRustにおいて特に意味はないが、構造体の新しいインスタンスを生成する関数としてよく使われる
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // new 関数を用意しないと以下のように呼び出さないといけなくなる
+    // let p1 = Philosopher { name: "Judith Butler".to_string() };
+    let p1 = Philosopher::new("Judith Butler");
+    let p2 = Philosopher::new("Gilles Deleuze");
+    let p3 = Philosopher::new("Karl Marx");
+    let p4 = Philosopher::new("Emma Goldman");
+    let p5 = Philosopher::new("Michel FouCault");
+
+    
+
 }
