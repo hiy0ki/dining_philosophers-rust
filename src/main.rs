12ad24f8ce17c8d758700bbf6431a712d36b7c6e
hiyoki <maha.tree.21@gmail.com>
2018-01-07 22:48:02 +0900

    add table

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::{Mutex, Arc};
 
 struct Philosopher {
     name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
     // newはRustにおいて特に意味はないが、構造体の新しいインスタンスを生成する関数としてよく使われる
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right: right,
         }
     }
 
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+        
         println!("{} is eating", self.name);
 
         thread::sleep(Duration::from_millis(1000));
@@ -22,20 +31,34 @@ impl Philosopher {
     }
 }
 
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
 fn main() {
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+    ]});
+    
     let philosophers = vec![
         // new 関数を用意しないと以下のように呼び出さないといけなくなる
         // let p1 = Philosopher { name: "Judith Butler".to_string() };
-        Philosopher::new("Judith Butler"),
-        Philosopher::new("Gilles Deleuze"),
-        Philosopher::new("Karl Marx"),
-        Philosopher::new("Emma Goldman"),
-        Philosopher::new("Michel FouCault"),
+        Philosopher::new("Judith Butler", 0, 1),
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Karl Marx", 2, 3),
+        Philosopher::new("Emma Goldman", 3, 4),
+        Philosopher::new("Michel FouCault", 0, 4),
     ];
 
     let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+        
         thread::spawn(move || {
-            p.eat();
+            p.eat(&table);
         })
     }).collect();
     
